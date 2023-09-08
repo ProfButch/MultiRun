8be70d346387b9800b15150cf74e802dcc652a40
profButch <butch.wesley+prof@gmail.com>
2023-09-08 11:21:35 -0400

    all features except log viewer

---
+++ MultiRun/Editor/MultiRunMenu.cs
@@ -0,0 +1,120 @@
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+
+namespace com.bitwesgames {
+    
+
+    [InitializeOnLoadAttribute]
+    public static class MultiRunMenu{
+
+        private static MultiRunEditor mre = new MultiRunEditor();
+
+
+        //--------------------------------
+        // Section 1
+        //--------------------------------
+        [MenuItem("MultiRun/Set Build Path", false, 1)]
+        private static void MnuSetBuildPath() {
+            string newPath = mre.getFilePathFromUser(mre.buildPath);
+            if(newPath.Length != 0)
+            {
+                mre.buildPath = newPath;
+            }
+        }
+
+
+        //--------------------------------
+        // Section 100
+        //--------------------------------
+        [MenuItem("MultiRun/Build", false, 100)]
+        private static void MnuBuildTheBuild() {
+            string curPath = mre.buildPath;
+            // This handles the one case where it wasn't set and the user canceled
+            // the dialog to set the build path.
+            if (curPath != string.Empty) {
+                mre.Build(curPath);
+            }
+        }
+
+
+        [MenuItem("MultiRun/Build Current Scene", false, 100)]
+        private static void MnuBuildCurrentScene() {
+            string curPath = mre.buildPath;
+            // This handles the one case where it wasn't set and the user canceled
+            // the dialog to set the build path.
+            if (curPath != string.Empty) {
+                mre.Build(curPath, true);
+            }
+        }
+
+
+        //--------------------------------
+        // Section 200
+        //--------------------------------
+        [MenuItem("MultiRun/Build & Run/1", false, 200)]
+        private static void MnuBuildRun1() {
+            mre.BuildThenRunX(1);
+        }
+
+        [MenuItem("MultiRun/Build & Run/2", false, 200)]
+        private static void MnuBuildRun2() {
+            mre.BuildThenRunX(2);
+        }
+
+        [MenuItem("MultiRun/Build & Run/3", false, 200)]
+        private static void MnuBuildRun3() {
+            mre.BuildThenRunX(3);
+        }
+
+        [MenuItem("MultiRun/Build & Run/4", false, 200)]
+        private static void MnuBuildRun4() {
+            mre.BuildThenRunX(4);
+        }
+
+        [MenuItem("MultiRun/Run/1", false, 200)]
+        private static void MnuRun1() {
+            mre.RunBuildXTimes(1);
+        }
+
+        [MenuItem("MultiRun/Run/2", false, 200)]
+        private static void MnuRun2() {
+            mre.RunBuildXTimes(2);
+        }
+
+        [MenuItem("MultiRun/Run/3", false, 200)]
+        private static void MnuRun3() {
+            mre.RunBuildXTimes(3);
+        }
+
+        [MenuItem("MultiRun/Run/4", false, 200)]
+        private static void MnuRun4() {
+            mre.RunBuildXTimes(4);
+        }
+
+
+        //--------------------------------
+        // Section 999
+        //--------------------------------
+        [MenuItem("MultiRun/Show Files", false, 999)]
+        private static void MnuViewFiles() {
+            string parentDir = Path.GetDirectoryName(mre.buildPath);
+            if (Directory.Exists(parentDir)) {
+                EditorUtility.RevealInFinder(mre.buildPath);
+            } else {
+                Debug.LogError($"Cannot open {parentDir} because it does not exist.  Kicking off a build will create the path.");
+            }
+        }
+
+
+        [MenuItem("MultiRun/About", false, 999)]
+        private static void MnuAbout(){
+            Debug.Log("About the thing.");
+            Debug.Log(mre.LazyGetBuildPath());
+        }
+    }
+}
---
+++ MultiRun/Editor/OsHelper.cs
@@ -0,0 +1,110 @@
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+namespace com.bitwesgames {
+    public class OsHelper
+    {
+        const string MODE_NONE = "none";
+        const string MODE_WIN = "win";
+        const string MODE_OSX = "osx";
+
+        public string mode = MODE_NONE;
+        public string execExtension = "";
+        public string defaultBuildPath = "";
+
+
+        public OsHelper()
+        {
+#if UNITY_EDITOR_WIN
+            InitWindows();
+#elif UNITY_EDITOR_OSX
+            InitOsx();
+#endif
+        }
+
+        private void InitWindows()
+        {
+            mode = MODE_WIN;
+            execExtension = "exe";
+            defaultBuildPath = "c:\\temp\\unity_builds\\TheBuild.exe";
+        }
+
+        private void InitOsx()
+        {
+            mode = MODE_OSX;
+            execExtension = "app";
+            defaultBuildPath = "~/temp/unity_builds/TheBuild.app";
+        }
+
+        private string RunBuildCmdWin(string path, string log)
+        {
+            // In windows, you have to give it the full path to where the log should
+            // go, it isn't cool like OSX.  So we have to construct the full path from
+            // the path to the executable.  Then it's pretty much the same as OSX.
+            string logPath = Path.GetDirectoryName(path);
+            string fullLogPath = Path.Join(logPath, log);
+
+            return $"{path} --logfile {fullLogPath}";
+        }
+
+        private string RunBuildCmdOsx(string path, string log)
+        {
+            // Uses open to launch the app so we do not have to know where the actual
+            // executable is inside the .app bundle.  This also sets the logfile to
+            // be the <app name>.log or whatever is specified in logfile.  The log
+            // will be in the same directory as the applicaiton.  Each new run
+            // overwrites the existing log file if it exists.
+            return $"open -n {path} --args --logfile {log}";
+        }
+
+        public string RunBuildCommand(string path, string log)
+        {
+            string toReturn = "";
+            if (mode == MODE_WIN)
+            {
+                toReturn = RunBuildCmdWin(path, log);
+            }
+            else if (mode == MODE_OSX)
+            {
+                toReturn = RunBuildCmdOsx(path, log);
+            }
+            return toReturn;
+        }
+
+        private BuildPlayerOptions BuildOptsOsx()
+        {
+            BuildPlayerOptions opts = new BuildPlayerOptions();
+            opts.target = BuildTarget.StandaloneOSX;
+            opts.targetGroup = BuildTargetGroup.Standalone;
+
+            return opts;
+        }
+
+        private BuildPlayerOptions BuildOptsWin()
+        {
+            BuildPlayerOptions opts = new BuildPlayerOptions();
+            opts.target = BuildTarget.StandaloneWindows;
+            opts.targetGroup = BuildTargetGroup.Standalone;
+
+            return opts;
+        }
+
+        public BuildPlayerOptions BuildOpts()
+        {
+            BuildPlayerOptions opts = new BuildPlayerOptions();
+            if (mode == MODE_WIN)
+            {
+                opts = BuildOptsWin();
+            }
+            else if (mode == MODE_OSX)
+            {
+                opts = BuildOptsOsx();
+            }
+            return opts;
+        }
+    }
+}
