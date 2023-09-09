b64a88f52b85529a5a0c305ae67d9ee943531919
profButch <butch.wesley+prof@gmail.com>
2023-09-09 13:50:57 -0400

    cleaner, needs better label/text chunking

--- MultiRun/Editor/LogViewer/LogDisplay.cs
+++ MultiRun/Editor/LogViewer/LogDisplay.cs
@@ -1,28 +1,33 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using UnityEditor.UIElements;
-using UnityEngine.UIElements;
-using UnityEngine;
-using System.Threading;
 using System.Text;
+using System.Threading;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 
 namespace com.bitwesgames
 {
     public class LogDisplay {
+        // -- Controls --
         public VisualElement root;
         public Label title;
         public Label logText;
         public ToolbarButton btnMaximize;
-
         private ScrollView scrollView;
         private Scroller vertScroll;
         private Scroller horizScroll;
 
+        // -- Other vars --
         private string logPath = string.Empty;
         private long lastFileSize = 0;
         private long lastReadLength = 0;
+
+        private Label curLabel = null;
+        private int labelCharLimit = 2000;
 
         public LogDisplay(VisualElement baseElement) {
             root = baseElement;
@@ -35,40 +40,19 @@ namespace com.bitwesgames
         }
 
 
-
-        // All this didn't seem to make much difference (it does some) when
-        // compared to making a label for each line.
-        //
-        // Maybe concatination is just as expensive as making a new lablel?
-        // On 7k lines in one log this shaves off about 1 second.  With
-        // 15k lines in two logs each, this shaves off maybe 5 seconds.
-        private Label curLabel = null;
-        private int lineLimit = 10;
-        private int linesAdded = 0;
-        private string textBuffer = string.Empty;
-        private void AddLineToBuffer(string text) {
-            if (curLabel == null || linesAdded > lineLimit) {
-                FlushLineBuffer();
-            }
-
-            if (linesAdded == 0) {
-                textBuffer = text;
+        private void AddTextToBuffer(string text) {
+            if(curLabel == null || text.Length + curLabel.text.Length > labelCharLimit){
+                curLabel = AddLabel(text);
             } else {
-                textBuffer += $"\n{text}";
-            }
-            linesAdded += 1;
-        }
-
-
-        private void FlushLineBuffer() {
-            if (textBuffer != string.Empty) {
-                curLabel = new Label();
-                AddLine(textBuffer);
-                linesAdded = 0;
-                textBuffer = string.Empty;
+                curLabel.text += text;
             }
         }
 
+        private Label AddLabel(string text){
+            var lbl = new Label(text);
+            scrollView.Add(lbl);
+            return lbl;
+        }
 
         private void AddLine(string text) {
             var lbl = new Label(text);
@@ -84,34 +68,10 @@ namespace com.bitwesgames
             }
         }
 
-        public void ScrollToBottom() {
-            vertScroll.value = vertScroll.highValue;
-            horizScroll.value = 0;
-        }
-
-
-        public void ScrollToTop() {
-            vertScroll.value = 0;
-            horizScroll.value = 0;
-        }
-
-
-
-        public void TailFile(string filePath){
-            logPath = filePath;
-            scrollView.Clear();
-            if (File.Exists(logPath)) {
-                lastReadLength = 0;
-                ReadToEndAndAddToDisplay();
-            } else {
-                AddLine("File not found");
-            }
-            UpdateTitle();
-        }
-
-
-        private string ReadToEnd(){
-            string toReturn = "";
+        /*
+         * Adapted from https://stackoverflow.com/questions/3791103/c-sharp-continuously-read-file
+         */
+        private void ReadToEnd(){
             try{
                 long fileSize = new FileInfo(logPath).Length;
                 if (fileSize > lastReadLength) {
@@ -125,41 +85,55 @@ namespace com.bitwesgames
                             lastReadLength += bytesRead;
                             foundData = bytesRead != 0;
                             if(foundData){
-                                toReturn += ASCIIEncoding.ASCII.GetString(buffer, 0, bytesRead);
+                                AddTextToBuffer(ASCIIEncoding.ASCII.GetString(buffer, 0, bytesRead));
                             }
                         }
                     }
                 }
             }
             catch { }
-
-            return toReturn;
         }
 
 
-        private void ReadToEndAndAddToDisplay(){
+        private void UpdateLogContents(){
             long fileSize = new FileInfo(logPath).Length;
             if(fileSize < lastFileSize){
                 scrollView.Clear();
                 lastReadLength = 0;
             }
             lastFileSize = fileSize;
+            ReadToEnd();
+        }
 
-            string data = ReadToEnd();
-            if(data.Length != 0){
-                string[] lines = data.Split("\n");
-                foreach (string line in lines){
-                    AddLineToBuffer(line);
-                }
-                FlushLineBuffer();
-                ScrollToBottom();
+
+        public void ScrollToBottom() {
+            vertScroll.value = vertScroll.highValue;
+            horizScroll.value = 0;
+        }
+
+
+        public void ScrollToTop() {
+            vertScroll.value = 0;
+            horizScroll.value = 0;
+        }
+
+
+        public void TailFile(string filePath){
+            logPath = filePath;
+            scrollView.Clear();
+            if (File.Exists(logPath)) {
+                lastReadLength = 0;
+                UpdateLogContents();
+            } else {
+                AddLine("File not found");
             }
+            UpdateTitle();
         }
 
 
         public void CallMeInUpdate(){
             if(logPath != string.Empty && File.Exists(logPath)){
-                ReadToEndAndAddToDisplay();
+                UpdateLogContents();
             } else{
                 scrollView.Clear();
                 UpdateTitle();
